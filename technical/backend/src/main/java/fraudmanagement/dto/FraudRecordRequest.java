d6daebc749fe240d20e3c8fde930cdbe560216cc
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:50:41 +0530

    Update FraudRecordRequest.java

--- technical/backend/src/main/java/fraudmanagement/dto/FraudRecordRequest.java
+++ technical/backend/src/main/java/fraudmanagement/dto/FraudRecordRequest.java
@@ -1,1 +1,27 @@
+package fraudmanagement.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import lombok.Data;
+
+@Data
+public class FraudRecordRequest {
+
+    @NotBlank(message = "Customer ID is required")
+    private String customerId;
+
+    @NotBlank(message = "Account number is required")
+    private String accountNumber;
+
+    @NotBlank(message = "Fraud type is required")
+    private String fraudType;
+
+    @NotNull(message = "Transaction amount is required")
+    @Positive(message = "Transaction amount must be greater than zero")
+    private Double transactionAmount;
+
+    @NotBlank(message = "Created by is required")
+    private String createdBy;
+}
