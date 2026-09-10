dd4883ce4dfc063f030e115ca42f7539f2cf383a
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:51:02 +0530

    Update FraudRecordResponse.java

--- technical/backend/src/main/java/fraudmanagement/dto/FraudRecordResponse.java
+++ technical/backend/src/main/java/fraudmanagement/dto/FraudRecordResponse.java
@@ -1,1 +1,36 @@
+package fraudmanagement.dto;
 
+import fraudmanagement.model.ApprovalStatus;
+import fraudmanagement.model.FraudStatus;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class FraudRecordResponse {
+
+    private String id;
+
+    private String customerId;
+
+    private String accountNumber;
+
+    private String fraudType;
+
+    private Double transactionAmount;
+
+    private FraudStatus fraudStatus;
+
+    private ApprovalStatus approvalStatus;
+
+    private String createdBy;
+
+    private String approvedBy;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
