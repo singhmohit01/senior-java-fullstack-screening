0cce9d846dfa3b29dc2dcb6010ec637ef6207faa
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:55:48 +0530

    Update GlobalExceptionHandler.java

--- technical/backend/src/main/java/fraudmanagement/exception/GlobalExceptionHandler.java
+++ technical/backend/src/main/java/fraudmanagement/exception/GlobalExceptionHandler.java
@@ -1,1 +1,54 @@
+package fraudmanagement.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(
+            FraudRecordNotFoundException.class
+    )
+    public ResponseEntity<ErrorResponse>
+    handleNotFound(
+            FraudRecordNotFoundException exception,
+            HttpServletRequest request) {
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.NOT_FOUND.value(),
+                        exception.getMessage(),
+                        request.getRequestURI()
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse>
+    handleGenericException(
+            Exception exception,
+            HttpServletRequest request) {
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "Internal server error",
+                        request.getRequestURI()
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+}
