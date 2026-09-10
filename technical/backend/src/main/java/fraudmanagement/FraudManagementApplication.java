ec2283426278d0f05b2a1e9b2825075bd0f34e94
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:48:51 +0530

    Update FraudManagementApplication.java

--- technical/backend/src/main/java/fraudmanagement/FraudManagementApplication.java
+++ technical/backend/src/main/java/fraudmanagement/FraudManagementApplication.java
@@ -1,1 +1,12 @@
-hello
+package fraudmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudManagementApplication.class, args);
+    }
+}
