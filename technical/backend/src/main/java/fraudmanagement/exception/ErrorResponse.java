5c14a0d2e02c6a94b8ae36783a8c7f4b74e2f28d
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:55:27 +0530

    Update ErrorResponse.java

--- technical/backend/src/main/java/fraudmanagement/exception/ErrorResponse.java
+++ technical/backend/src/main/java/fraudmanagement/exception/ErrorResponse.java
@@ -1,1 +1,19 @@
+package fraudmanagement.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+
+    private int status;
+
+    private String message;
+
+    private String path;
+}
