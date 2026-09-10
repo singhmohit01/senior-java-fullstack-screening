9181f260be804b1a2d42c58de3b235f332bd2266
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:46:58 +0530

    Update FraudRecord.java

--- technical/backend/src/main/java/fraudmanagement/model/FraudRecord.java
+++ technical/backend/src/main/java/fraudmanagement/model/FraudRecord.java
@@ -1,1 +1,35 @@
+package fraudmanagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "fraud_records")
+public class FraudRecord {
+
+    @Id
+    private String id;
+
+    private String customerId;
+    private String accountNumber;
+    private String fraudType;
+    private Double transactionAmount;
+
+    private FraudStatus fraudStatus;
+    private ApprovalStatus approvalStatus;
+
+    private String createdBy;
+    private String approvedBy;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
