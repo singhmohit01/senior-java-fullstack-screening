715d14d3810663d4457498ce7d2dddd342736116
singhmohit01 <smohit9328@gmail.com>
2026-09-10 14:51:48 +0530

    Update FraudRecordRepository.java

--- technical/backend/src/main/java/fraudmanagement/repository/FraudRecordRepository.java
+++ technical/backend/src/main/java/fraudmanagement/repository/FraudRecordRepository.java
@@ -1,1 +1,18 @@
+package fraudmanagement.repository;
 
+import fraudmanagement.model.ApprovalStatus;
+import fraudmanagement.model.FraudRecord;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface FraudRecordRepository
+        extends MongoRepository<FraudRecord, String> {
+
+    List<FraudRecord> findByCustomerId(String customerId);
+
+    List<FraudRecord> findByApprovalStatus(
+            ApprovalStatus approvalStatus
+    );
+}
